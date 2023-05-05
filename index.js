fcd80b7e4ce89b337e6d6a347d879dabee5b9493
csmahmud99 <csmahmud.2021@gmail.com>
2023-05-05 16:48:02 +0600

    Chef bio is re-written

--- index.js
+++ index.js
@@ -7,20 +7,24 @@ const chefRecipe = require("./data/chefRecipe.json");
 
 app.use(cors());
 
+// General Output API from Server
 app.get("/", (req, res) => {
     res.send("'Yumma' is Running");
 });
 
+// This API is Providing All Data to the Client
 app.get("/chef-recipe", (req,res) => {
     res.send(chefRecipe);
 });
 
+// This API is Providing Data to the Client According to the User's Choice
 app.get("/chef-recipe/:id", (req, res) => {
     const id = parseInt(req.params.id);
     const items = chefRecipe.find(item => parseInt(item.chefId) === id);
     res.send(items);
 })
 
+// App is running or not inside production
 app.listen(port, () => {
     console.log(`'Yumma' API is running on port: ${port}`);
 });
